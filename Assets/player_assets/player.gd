7369bf820ae4b8a49e0b086eb7f6f41f16edcb63
DAlexander5279 <104796691+DAlexander5279@users.noreply.github.com>
2024-04-10 18:28:00 -0400

    Running Animation and state
    created an animation when player is moving

--- Assets/player_assets/player.gd
+++ Assets/player_assets/player.gd
@@ -10,7 +10,7 @@ extends CharacterBody2D
 @export var current_jumps : int = 0
 
 #State machine variables
-enum State{Idle}
+enum State{Idle, Running}
 var current_state : State 
 
 #calls on start
@@ -21,6 +21,7 @@ func _ready():
 #controls all physics processes
 func _physics_process(delta):
 	player_falling(delta)
+	player_idle(delta)
 	player_move(delta)
 	player_jump(delta)
 	move_and_slide()
@@ -32,7 +33,9 @@ func player_falling(delta):
 	if !is_on_floor():
 		velocity.y += gravityForce * delta
 		
-		
+func player_idle(delta):
+	if is_on_floor():
+		current_state = State.Idle
 #moves player based on input direction
 func player_move(delta):
 	
@@ -43,6 +46,7 @@ func player_move(delta):
 		
 		
 	if movement_direction() != 0:
+		current_state = State.Running
 		if movement_direction() > 0:
 			player_sprite.flip_h = false
 		else:
@@ -63,6 +67,8 @@ func play_animations():
 	match current_state:
 		State.Idle:
 			player_sprite.play("idle")
+		State.Running:
+			player_sprite.play("running")
 #returns variable based on input direction
 func movement_direction():
 	var direction = Input.get_axis("move_left", "move_right")
