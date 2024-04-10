2652d3b1f8ffc8acfe99ad3b935c774bbd15c328
DAlexander5279 <104796691+DAlexander5279@users.noreply.github.com>
2024-04-10 18:17:26 -0400

    State machine for animations
    initialized the state machine that controls animations, starting with idle

--- Assets/player_assets/player.gd
+++ Assets/player_assets/player.gd
@@ -1,25 +1,39 @@
 extends CharacterBody2D
+#reference to sprite
+@onready var player_sprite = $"AnimatedSprite2D"
 
-@onready var player_sprite = $"Player-idle-1"
-
+#variables controlling gravity and movement
 @export var gravityForce : int = 1000
 @export var playerSpeed : int = 300
 @export var jumpForce : int = -300
 @export var max_jumps : int = 1
 @export var current_jumps : int = 0
 
+#State machine variables
+enum State{Idle}
+var current_state : State 
+
+#calls on start
+func _ready():
+	current_state = State.Idle
+
+
+#controls all physics processes
 func _physics_process(delta):
 	player_falling(delta)
 	player_move(delta)
 	player_jump(delta)
 	move_and_slide()
+	play_animations()
 
 
-
+#pushes player down by a force
 func player_falling(delta):
 	if !is_on_floor():
 		velocity.y += gravityForce * delta
 		
+		
+#moves player based on input direction
 func player_move(delta):
 	
 	if movement_direction():
@@ -34,6 +48,8 @@ func player_move(delta):
 		else:
 			player_sprite.flip_h = true
 			
+
+#pushes a force upwarard if conditions are met
 func player_jump(delta):
 	if is_on_floor():
 		current_jumps = 0
@@ -42,6 +58,12 @@ func player_jump(delta):
 		velocity.y = jumpForce
 		current_jumps = current_jumps + 1
 	
+	
+func play_animations():
+	match current_state:
+		State.Idle:
+			player_sprite.play("idle")
+#returns variable based on input direction
 func movement_direction():
 	var direction = Input.get_axis("move_left", "move_right")
 	return direction
