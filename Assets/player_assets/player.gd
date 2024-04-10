8d39798b8c77f8c3f28341c1314c8b545a6fc7b3
DAlexander5279 <104796691+DAlexander5279@users.noreply.github.com>
2024-04-09 21:20:50 -0400

    Horizontal movement
    Player can move left and right with A and D keys, sprite will also flip based on direction

--- Assets/player_assets/player.gd
+++ Assets/player_assets/player.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
+@onready var player_sprite = $"Player-idle-1"
+
 @export var gravityForce = 1000
+@export var playerSpeed = 300
 
 func _physics_process(delta):
 	player_falling(delta)
+	player_move(delta)
 	move_and_slide()
 
 
@@ -11,3 +15,18 @@ func _physics_process(delta):
 func player_falling(delta):
 	if !is_on_floor():
 		velocity.y += gravityForce * delta
+		
+func player_move(delta):
+	
+	if movement_direction():
+		velocity.x = movement_direction() * playerSpeed
+	else:
+		velocity.x = move_toward(velocity.x, 0 , playerSpeed)
+		
+		
+	if movement_direction() != 0:
+		player_sprite.flip_h = false if movement_direction() > 0 else true
+	
+func movement_direction():
+	var direction = Input.get_axis("move_left", "move_right")
+	return direction
