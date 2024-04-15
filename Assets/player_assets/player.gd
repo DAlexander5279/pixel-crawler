df494dc7f4afd8222168db0f3f03bd9eccfc7b69
DAlexander5279 <104796691+DAlexander5279@users.noreply.github.com>
2024-04-14 21:25:41 -0400

    Bullet instantiation and run-shooting animation

--- Assets/player_assets/player.gd
+++ Assets/player_assets/player.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 #reference to sprite
 @onready var player_sprite = $"AnimatedSprite2D"
+#bullet reference
+@onready var bulletMarker : Marker2D = $FirePoint
+var projectile_spawn_point
+var PrimaryProjectile = preload("res://Assets/player_assets/projectiles/projectile_1.tscn")
 
 #variables controlling gravity and movement
 @export var gravityForce : int = 1000
@@ -14,12 +18,13 @@ extends CharacterBody2D
 @export var current_health : int = 100
 
 #State machine variables
-enum State{Idle, Running, Jump}
+enum State{Idle, Running, Jump, Shooting}
 var current_state : State 
 
 #calls on start
 func _ready():
 	current_state = State.Idle
+	projectile_spawn_point = bulletMarker.position
 
 
 #controls all physics processes
@@ -28,14 +33,31 @@ func _physics_process(delta):
 	player_idle(delta)
 	player_move(delta)
 	player_jump(delta)
+	flip_projectile_position()
+	shoot(delta)
 	move_and_slide()
 	play_animations()
 
 
+func flip_projectile_position():
+	var direction = movement_direction()
+	if direction > 0:
+		bulletMarker.position.x = projectile_spawn_point.x
+	else:
+		bulletMarker.position.x = -projectile_spawn_point.x
 
+func shoot(delta):
+	var direction = movement_direction()
+	if direction and Input.is_action_just_pressed("PrimaryFire"):
+		current_state = State.Shooting
+		var primary_proj = PrimaryProjectile.instantiate() as Node2D
+		#adds bullet as a child to the level
+		primary_proj.global_position = bulletMarker.global_position
+		primary_proj.direction = direction
+		get_parent().add_child(primary_proj)
+		
+		
 #function to take damage : called by enemy projectiles
-
-
 func player_take_damage(amount):
 	current_health = current_health - amount
 
@@ -81,9 +103,12 @@ func play_animations():
 		State.Idle:
 			player_sprite.play("idle")
 		State.Running:
-			player_sprite.play("running")
+			if player_sprite.animation != "run_shooting":
+				player_sprite.play("running")
 		State.Jump:
 			player_sprite.play("jump")
+		State.Shooting:
+			player_sprite.play("run_shooting")
 #returns variable based on input direction
 func movement_direction():
 	var direction = Input.get_axis("move_left", "move_right")
