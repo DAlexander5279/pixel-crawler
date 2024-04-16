b3ed863e43ece8be3e416a1f712f5182bc7cfc77
DAlexander5279 <104796691+DAlexander5279@users.noreply.github.com>
2024-04-16 18:12:18 -0400

    Enemy patrolling and collision detection

--- Assets/Enemies/Crab/crab.gd
+++ Assets/Enemies/Crab/crab.gd
@@ -1,7 +1,17 @@
 extends CharacterBody2D
+
+#sprite reference
+@onready var sprite = $AnimatedSprite2D
+
+#movement variables
 @export var gravityForce : int = 1000
 @export var SPEED : int = 3000
 var direction : Vector2 = Vector2.LEFT 
+
+#raycast variables
+@onready var ledgeCheckleft = $LedgeCheckLeft
+@onready var ledgeCheckRight = $LedgeCheckRight
+
 
 enum State {Idle, Walk}
 var current_state : State
@@ -11,12 +21,21 @@ func _ready():
 
 
 func _physics_process(delta):
+	checkCollisions()
 	applyGravity(delta)
 	idleing(delta)
 	walking(delta)
 	move_and_slide()
 	
 	
+	
+	
+func checkCollisions():
+	var found_wall = is_on_wall()
+	var found_ledge = not ledgeCheckleft.is_colliding() or not ledgeCheckRight.is_colliding()
+	if found_wall or found_ledge:
+		direction.x *= -1
+	sprite.flip_h = direction.x > 0
 	
 func applyGravity(delta):
 	if !is_on_floor():
@@ -29,3 +48,4 @@ func idleing(delta):
 func walking(delta):
 	current_state = State.Walk
 	velocity.x = direction.x * SPEED * delta
+	
