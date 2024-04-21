75a0ab04872c590cf829ca8a7387ba1760f24f5c
DAlexander5279 <104796691+DAlexander5279@users.noreply.github.com>
2024-04-21 15:21:31 -0400

    Enemy taking damage, and death effect

--- Assets/player_assets/projectiles/projectile_1.gd
+++ Assets/player_assets/projectiles/projectile_1.gd
@@ -4,6 +4,7 @@ var bulletEffect  = preload("res://Assets/player_assets/projectiles/impact_effec
 
 var direction
 @export var speed = 600
+@export var damage = 1
 
 func _physics_process(delta):
 	move_local_x(direction * speed * delta)
@@ -19,9 +20,12 @@ func _on_hitbox_area_entered(area):
 func _on_hitbox_body_entered(body):
 	impact()
 	
-	
+
 func impact():
 	var bulletEffect_spawn = bulletEffect.instantiate() as Node2D
 	bulletEffect_spawn.global_position = global_position
 	get_parent().add_child(bulletEffect_spawn)
 	queue_free()
+	
+func get_damage() -> int:
+	return damage
