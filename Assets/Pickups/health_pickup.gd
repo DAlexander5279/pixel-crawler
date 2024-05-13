5c6623a48d83d88080e0e8fc73e7cb7b324ee817
DAlexander5279 <104796691+DAlexander5279@users.noreply.github.com>
2024-05-13 14:20:26 -0400

    Player Shader outline

--- Assets/Pickups/health_pickup.gd
+++ Assets/Pickups/health_pickup.gd
@@ -4,10 +4,12 @@ extends Node2D
 @onready var collider = $Area2D/CollisionShape2D
 @onready var sprite = $"Health-red32Px"
 
+var triggered = false
 
 
 func _on_area_2d_body_entered(body : Node2D):
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player") and triggered == false:
+		triggered = true
 		HealthManager.add_Health(1)
 		collider.disabled = true
 		sprite.visible = false
