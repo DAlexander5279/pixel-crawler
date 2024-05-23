9fb96c460b519190042bb829f6bdd35a9546baf2
Cobi Scherb <cjscherb@student.fullsail.edu>
2024-05-23 10:40:40 -0400

    Boss Fight Music
    Added to the trigger a function to change the music when entering the boss fight arena

--- Scripts/Trigger.gd
+++ Scripts/Trigger.gd
@@ -2,6 +2,10 @@ extends Area2D
 
 signal PlayerEntered
 
+@onready var main_background_music = $"../Main Background Music" as AudioStreamPlayer
+@onready var boss_fight_music = $"../Boss Fight Music" as AudioStreamPlayer
+
+
 #function to see when player enters trigger space
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
@@ -11,4 +15,10 @@ func _physics_process(delta):
 			queue_free()
 
 func _ready():
-	pass # Replace with function body.
+# Connect the PlayerEntered signal to the _on_PlayerEntered function
+	connect("PlayerEntered", Callable(self, "_on_PlayerEntered"))
+
+# Function to handle the PlayerEntered signal
+func _on_PlayerEntered():
+	main_background_music.stop()
+	boss_fight_music.play()
