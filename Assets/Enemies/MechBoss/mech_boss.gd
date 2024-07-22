fdda36b256b1d0d04bc8622e97771be52e641254
DAlexander5279 <104796691+DAlexander5279@users.noreply.github.com>
2024-07-22 17:10:48 -0400

    boss damage and state machine

---
+++ Assets/Enemies/MechBoss/mech_boss.gd
@@ -0,0 +1,72 @@
+extends CharacterBody2D
+
+
+@export var health = 90
+
+@onready var base_sprite = $"Mech/upperBoddy/Marker2D/SpriteTorso"
+@onready var phaseTimer : Timer = $phaseTimer
+@onready var phase_player = $"AnimationPlayer"
+var isAttacking = false
+
+func _ready():
+	randomize()
+
+func _physics_process(delta):
+	checkHealth()
+	attackPhase()
+	
+func attackPhase():
+	
+	if !isAttacking:
+		var random_number = randi() % 3 + 1
+		print(random_number)
+		isAttacking = true
+		print("entered")
+		match random_number:
+			1:
+				phase_player.play("fist_smash")
+				print("entered phase")
+				phaseTimer.start(2)
+			2:
+				phase_player.play("laser")
+				phaseTimer.start(3.9)
+				print("entered phase")
+			3:
+				phase_player.play("ground smash")
+				phaseTimer.start(2)
+				print("entered phase")
+		
+	
+func checkHealth():
+	if health < 30:
+		base_sprite.play(("LowHealth"))
+	elif  health < 60:
+		base_sprite.play("MidHealth")
+	else:
+		base_sprite.play("FullHealth")
+		
+
+
+
+		
+		
+		
+	
+var idle = false
+
+func _on_phase_timer_timeout():
+	if idle == false:
+		idle = true
+		phase_player.play("idling")
+		phaseTimer.start(3)
+	else:
+		isAttacking = false
+		idle = false
+
+
+func _on_hit_box_area_entered(area):
+	if(area.get_parent().has_method("get_damage")):
+		var bullet = area.get_parent() as Node
+		health -= bullet.damage
+		if(health <= 0):
+			queue_free()
